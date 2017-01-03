cab0b0501e77384b1b9eade7b01a08e56959a917
Monte Thakkar <mthakkar@mail.sfsu.edu>
2017-01-03 19:26:55 +0530

    updated views & added bg image

--- SocialStarter/index.ios.js
+++ SocialStarter/index.ios.js
@@ -5,12 +5,17 @@ import {
   Text,
   View,
   Alert,
+  Image,
+  Dimensions,
   ScrollView
 } from 'react-native';
 
 import { FBLogin, FBLoginManager } from 'react-native-facebook-login';
 
 import ProfileView from './app/ProfileView'
+
+const { width, height } = Dimensions.get("window");
+const background = require("./app/bg.jpg");
 
 export default class SocialStarter extends Component {
   constructor () {
@@ -32,61 +37,65 @@ export default class SocialStarter extends Component {
     var user = this.state.user;
 
     return (
-      <ScrollView style={styles.container}>
+      <View style={styles.container}>
+        <ScrollView style={styles.profileViewContainer}>
+          { user && <ProfileView user={user} onLogout={this.onLogout.bind(this)}/> }
+        </ScrollView>
 
-        { user && <ProfileView user={user} onLogout={this.onLogout.bind(this)}/> }
-        <View style={styles.logoutContainer}>
-          { !user &&
-          <FBLogin
-            style={{ marginBottom: 10, }}
-            permissions={["email","user_friends"]}
-            onLogin={function(data){
-              console.log("Logged in!");
-              console.log(data);
-              _this.setState({ user : data.credentials });
-            }}
-            onLogout={function(){
-              console.log("Logged out.");
-              _this.setState({ user : null });
-            }}
-            onLoginFound={function(data){
-              console.log("Existing login found.");
-              console.log(data);
-              _this.setState({ user : data.credentials });
-            }}
-            onLoginNotFound={function(){
-              console.log("No user logged in.");
-              _this.setState({ user : null });
-            }}
-            onError={function(data){
-              console.log("ERROR");
-              console.log(data);
-            }}
-            onCancel={function(){
-              console.log("User cancelled.");
-            }}
-            onPermissionsMissing={function(data){
-              console.log("Check permissions!");
-              console.log(data);
-            }}
-          />}
-        </View>
-      </ScrollView>
+        { !user && <View style={styles.facebookLoginContainer}>
+          <Image source={background} style={styles.bgImage} >
+            <FBLogin
+              style={styles.loginButton}
+              permissions={["email","user_friends"]}
+              onLogin={function(data){
+                console.log("Logged in!");
+                console.log(data);
+                _this.setState({ user : data.credentials });
+              }}
+              onLogout={function(){
+                console.log("Logged out.");
+                _this.setState({ user : null });
+              }}
+              onLoginFound={function(data){
+                console.log("Existing login found.");
+                console.log(data);
+                _this.setState({ user : data.credentials });
+              }}
+              onLoginNotFound={function(){
+                console.log("No user logged in.");
+                _this.setState({ user : null });
+              }}
+              onError={function(data){
+                console.log("ERROR");
+                console.log(data);
+              }}
+              onCancel={function(){
+                console.log("User cancelled.");
+              }}
+              onPermissionsMissing={function(data){
+                console.log("Check permissions!");
+                console.log(data);
+              }}
+            />
+          </Image>
+        </View>}
+      </View>
     );
   }
 }
 
 var styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#e1e8ee',
+    backgroundColor: '#e1e8ee'
   },
-  logoutContainer: {
-    alignItems: 'center',
-    alignSelf: 'center',
-    justifyContent: 'center',
-    paddingTop: 20,
-    paddingBottom: 20,
+  bgImage: {
+    width,
+    height,
+  },
+  loginButton: {
+    position: 'absolute',
+    left: width / 2 - 80,
+    top: height / 2 - 10,
   }
 });
 
